fe5824b11f6bd085300d9c12efc4afe4a9d04b21
John Phonxaya <phonxayajohn@gmail.com>
2023-06-14 18:17:14 -0500

    props basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useSyncExternalStore } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name="Liza" 
+        city="New York" 
+        color="firebrick" 
+      />
+      <About 
+        bio={user.bio} links={user.links}
+      />
     </div>
   );
 }
